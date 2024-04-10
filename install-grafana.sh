92c5c9d8104027bbdb811a997af66c368afb999b
Ayush Pareek <126587776+Ayush1-Explorer@users.noreply.github.com>
2024-04-11 00:19:14 +0530

    Update install-grafana.sh

--- install-grafana.sh
+++ install-grafana.sh
@@ -1,7 +1,7 @@
-sudo apt-get install -y adduser libfontconfig1 musl
-wget https://dl.grafana.com/oss/release/grafana_10.4.1_arm64.deb
-sudo dpkg -i grafana_10.4.1_arm64.deb
-sudo systemctl daemon-reload
-sudo systemctl start grafana-server
-sudo systemctl status grafana-server
-sudo systemctl enable grafana-server.service
+sudo apt-get install -y apt-transport-https software-properties-common wget
+sudo mkdir -p /etc/apt/keyrings/
+wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+sudo apt-get update
+sudo apt-get install grafana
+sudo systemctl status grafana
