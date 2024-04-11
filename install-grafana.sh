2f542200bf982a97444b26758055176adef01d0d
Ayush Pareek <126587776+Ayush1-Explorer@users.noreply.github.com>
2024-04-11 09:20:17 +0530

    Update install-grafana.sh

--- install-grafana.sh
+++ install-grafana.sh
@@ -4,4 +4,5 @@ wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt
 echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 sudo apt-get update
 sudo apt-get install grafana
+sudo systemctl enable grafana
 sudo systemctl status grafana
